e791d47f80f128ee0cad84151605179e2b2e4ca9
s78740 <s78740@beuth-hochschule.de>
2020-10-29 17:10:53 +0100

    changes

---
+++ scripts/private.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function revealMessage(){
+    document.getElementById("hiddenMessage").style.display= 'block';
+    document.getElementById("read").innerHTML=" ";
+}
+function revealMessage2(){
+    document.getElementById("hiddenMessage2").style.display= 'block';
+     document.getElementById("read2").innerHTML=" ";
+}
+
